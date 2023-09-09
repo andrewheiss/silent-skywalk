97981ec40ad3831a3e83cd14ff90c5a4e1a7a765
Andrew Heiss <andrew@andrewheiss.com>
2023-09-09 15:22:12 -0400

    Wider histogram bins

--- R/funs_plots.R
+++ R/funs_plots.R
@@ -17,7 +17,7 @@ plot_trank <- function(model, params) {
     group_by(.variable) %>% 
     mutate(draw_rank = rank(.value)) %>% 
     ggplot(aes(x = draw_rank, color = factor(.chain))) +
-    stat_bin(geom = "step", binwidth = 200, position = position_identity(), boundary = 0) +
+    stat_bin(geom = "step", binwidth = 400, position = position_identity(), boundary = 0) +
     scale_color_viridis_d(option = "rocket", end = 0.85) +
     labs(color = "Chain") +
     facet_wrap(vars(.variable), scales = "free_y") +
